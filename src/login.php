50c0e0d86ecba6a05bb708b7864b6a789cd0acc2
Tronaluz <lucas_tron@hotmail.com>
2022-12-02 23:13:20 -0300

    subindo

--- src/login.php
+++ src/login.php
@@ -1,40 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title>log</title>
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+   <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+      <title>log</title>
 
 
-</head>
-<body>
-<section style="background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 140, 71))" class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+   </head>
+   <body>
+   <section style="background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 140, 71))" class="vh-100" style="background-color: #eee;">
+   <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+         <div class="col-lg-12 col-xl-11">
+         <div class="card text-black" style="border-radius: 25px;">
+            <div class="card-body p-md-5">
+               <div class="row justify-content-center">
+               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-<form>
-  <div style= "text-align: center" class="form-group" >
-    <input style="background-color: white; border:solid 1px; border: radius 15px; width:300px;" type="email" class="form-control" id="exampleInputEmail1"  placeholder="Email">
-  </div>
-  <div style= "text-align: center" class="form-group">
-    <input style="margin-top: 25px; background-color: white; border:solid 1px; border-radius:15px; width:300px;" type="senha" class="form-control" id="Senha" placeholder="Senha">
-  </div>
-  <div style= "text-align: center" class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
-  </div>
-  <div style= "text-align: center">
-    <button type="Entrar" class="btn btn-primary">Entrar</button>
-    <button style="margin-left: 10px" type="Registro" class="btn btn-primary">Registrar</button>
-  </div>
-</form>
+   <form>
+   <div class="form-group" >
+      <input style="background-color: white; border:solid 1px; border: radius 15px; width:300px; text-align: left" type="email" class="form-control" id="exampleInputEmail1"  placeholder="Email">
+   </div>
+   <div  class="form-group">
+      <input style="margin-top: 25px; background-color: white; border:solid 1px; border-radius:15px; text-align: left" type="senha" class="form-control" id="Senha" placeholder="Senha">
+   </div>
+   <div class="form-check">
+      <input style= " text-align: left type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label style= " text-align: left class="form-check-label" for="exampleCheck1">Lembrar-me</label>
+   </div>
+   <div style= "text-align: center">
+      <button type="Entrar" class="btn btn-primary">Entrar</button>
+      <button style="margin-left: 10px" type="Registro" class="btn btn-primary">Registrar</button>
+   </div>
+   </form>
 
-</body>
-</html>
+   </body>
+   </html>
